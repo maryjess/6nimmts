c50e40798ac233b44e61a209701a1964212c647e
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2024-08-27 12:58:35 +0530

    Revert "Restructure and distribute functions #16"
    This reverts commit afe0864ac7860a8b5cc8341c80dbe3f192e11cab.

--- printing.py
+++
@@ -1,7 +0,0 @@
-def print_deck(deck):
-    for row in deck:
-        print(row)
-
-def print_players(players):
-    for player, hand in players.items():
-        print(f"Player {player}'s hand: {hand}")
